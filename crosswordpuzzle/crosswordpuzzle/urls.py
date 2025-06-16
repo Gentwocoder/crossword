27aeeda3b98a9ec9a5f09ccd971aafa4c3461578
Gentwocoder <adetoyese0511@gmail.com>
2025-06-16 01:52:15 +0100

    restricted access

--- crosswordpuzzle/crosswordpuzzle/urls.py
+++ crosswordpuzzle/crosswordpuzzle/urls.py
@@ -1,6 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseForbidden
+
+def ip_restriction_middleware(get_response):
+    def middleware(request):
+        allowed_ips = ['10.194.160.32', '192.168.101.237', '10.103.90.2']
+        client_ip = request.META.get('REMOTE_ADDR')
+        if client_ip not in allowed_ips:
+            return HttpResponseForbidden("Access denied")
+        return get_response(request)
+    return middleware
 
 urlpatterns = [
     path('admin/', admin.site.urls),
